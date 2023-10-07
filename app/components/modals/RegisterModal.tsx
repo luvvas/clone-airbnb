fe0676f89d077e8477727f5febded75d654273dd
luvvas <lucasmachado1@ufpr.br>
2023-10-07 04:54:21 -0300

    feat: add footer register modal component

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -5,14 +5,16 @@ import { AiFillGithub } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import toast from 'react-hot-toast';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal'
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
+import Button from '../Button';
 
 const RegisterModal = () => {
-  const registerModel = useRegisterModal();
+  const registerModal = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false)
 
   const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>({
@@ -28,10 +30,10 @@ const RegisterModal = () => {
 
     axios.post('/api/register', data)
       .then(() => {
-        registerModel.onClose()
+        registerModal.onClose()
       })
       .catch((error) => {
-        console.log(error)
+        toast.error('Alguma coisa deu errado')
       })
       .finally(() => {
         setIsLoading(false)
@@ -47,15 +49,47 @@ const RegisterModal = () => {
     </div>
   )
 
+  const footerContent = (
+    <div className='flex flex-col gap-4 mt-3'>
+      <hr />
+      <Button 
+        outline 
+        label="Continuar com Google" 
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button 
+        outline 
+        label="Continuar com Github" 
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className='justify-center text-center flex flex-row items-center gap-2'>
+          <div>
+            Já tem uma conta?
+          </div>
+          <div 
+            onClick={registerModal.onClose}
+            className="text-neutral-800 cursor-pointer hover:underline"
+          >
+            Entrar
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <Modal 
       disabled={isLoading}
-      isOpen={registerModel.isOpen}
+      isOpen={registerModal.isOpen}
       title="Register"
       actionLabel="Continue"
-      onClose={registerModel.onClose}
+      onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   )
 }
