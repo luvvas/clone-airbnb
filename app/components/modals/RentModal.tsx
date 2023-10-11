d62eab8b5cb16ffa6892e280fc21e5ede5da2431
luvvas <lucasmachado1@ufpr.br>
2023-10-11 16:30:27 -0300

    feat: add image upload step

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../ImageUpload';
 import dynamic from 'next/dynamic';
 
 enum STEPS {
@@ -55,6 +56,7 @@ const RentModal = () => {
   const guestCount = watch('guestCount')
   const roomCount = watch('roomCount')
   const bathroomCount = watch('bathroomCount')
+  const imageSrc = watch('imageSrc')
 
   const Map = useMemo(() => dynamic(() => import('../Map'), {
     ssr: false
@@ -153,6 +155,21 @@ const RentModal = () => {
           subtitle="Quantos banheiro você tem?"
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
+      </div>
+    )
+  }
+
+  if(step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8"> 
+        <Heading 
+          title="Adicione uma foto do seu espaço"
+          subtitle="Mostre aos seus convidados como é o seu espaço!"
+        />
+        <ImageUpload 
+          value={imageSrc}
+          onChange={(value) => setCustomValue('imageSrc', value)}
         />
       </div>
     )
