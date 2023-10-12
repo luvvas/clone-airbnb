acaaf1750875b5696d1c890a1a64d3163574e282
luvvas <lucasmachado1@ufpr.br>
2023-10-12 11:07:48 -0300

    feat: add dynamic get all listings without api request

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import getListings from "./actions/getListings";
+
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings()
 
-  if(isEmpty) {
+  if(listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset/>
@@ -17,7 +19,11 @@ export default function Home() {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          <div>My future listings</div>
+          {listings.map((listing: any) => {
+            return (
+              <div>{listing.title}</div>
+            )
+          })}
         </div>
       </Container>
     </ClientOnly>
